b59a03073801e6ba740d6e4f1e27d423616c7420
talrez11 <tal@way2cu.com>
2015-11-12 22:00:57 +0300

    Script: added scroll function

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -110,6 +110,20 @@ Site.on_load = function() {
 
 	 // create fixed position menu
 	Site.menu = new FloatingMenu($('div.menu'), $('section.about'));
+
+	//Scroll Function
+	$('a[href*=#]').bind('click', function(e) {
+		e.preventDefault(); //prevent the "normal" behaviour which would be a "hard" jump
+
+		var target = $(this).attr("href"); //Get the target
+
+	// perform animated scrolling by getting top-position of target-element and set it as scroll target
+		$('html, body').stop().animate({ scrollTop: $(target).offset().top - 120 }, 1000, function() {
+		     location.hash = target;  //attach the hash (#jumptarget) to the pageurl
+		});
+
+		return false;
+	   });
 };
 
 
