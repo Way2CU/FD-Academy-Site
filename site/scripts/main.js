39cd53f17cb0cfaa76fa899f83a02c12f5a54b31
talrez11 <tal@way2cu.com>
2015-11-15 13:12:49 +0300

    Script: updated mobile slider function

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -96,18 +96,18 @@ Site.on_load = function() {
 	if (Site.is_mobile()) 
 		Site.mobile_menu = new Caracal.MobileMenu();
 
-	if (!Site.is_mobile()) {
+	if (Site.is_mobile()) {
 
-		 Site.client_logo_slider_mobile = new Caracal.Gallery.Slider();
-		 Site.client_logo_slider_mobile
+		 Site.client_logo_slider = new Caracal.Gallery.Slider();
+		 Site.client_logo_slider
 			.images.set_container('div.slider')
-			.images.set_visible_count(5)
+			.images.set_visible_count(1)
 			.images.set_step_size(1)
 			.images.set_center(true)
 			.images.add('div.slider img')
 			.controls.attach_next(' a.next')
 			.controls.attach_previous(' a.previous');
-		 Site.client_logo_slider_mobile.images.update();
+		 Site.client_logo_slider.images.update();
 
 	}
 
@@ -115,7 +115,7 @@ Site.on_load = function() {
 	 Site.client_logo_slider_mobile = new Caracal.Gallery.Slider();
 	 Site.client_logo_slider_mobile
 		.images.set_container('div.slider')
-		.images.set_visible_count(1)
+		.images.set_visible_count(5)
 		.images.set_step_size(1)
 		.images.set_center(true)
 		.images.add('div.slider img')
