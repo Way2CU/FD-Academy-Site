7372defbe507265de7c7b37faa7f3e9196ec150f
talrez11 <tal@way2cu.com>
2015-11-15 22:36:31 +0300

    Script: removed floating menu function

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -46,50 +46,6 @@ Site.is_mobile = function() {
 };
 
 /**
- * @param object menu               jQuery object
- * @param object trigger_element    jQuery object
- */
-function FloatingMenu(menu, trigger_element){
-    var self = this;
-
-    self.menu = menu;
-    self.position = trigger_element.offset().top;
-    self.active = false;
-
-    /**
-     * Object initialization.
-     */
-    self._init = function() {
-        // connect signals
-        $(window).on('scroll', self.handle_scroll);
-
-        // set initial state
-        self.handle_scroll(null);
-    };
-
-    /**
-     * Handle window scroll.
-     *
-     * @param object event
-     */
-    self.handle_scroll = function(event) {
-        var over_position = $(window).scrollTop() >= self.position;
-
-        if (over_position && !self.active) {
-            self.menu.addClass('active');
-            self.active = true;
-
-        } else if (!over_position && self.active) {
-            self.menu.removeClass('active');
-            self.active = false;
-        }
-    };
-
-    // finalize object
-    self._init();
-}
-
-/**
  * Function called when document and images have been completely loaded.
  */
 Site.on_load = function() {
@@ -134,8 +90,6 @@ Site.on_load = function() {
 	                .images.set_center(true)
 	}
 
-	 // create fixed position menu
-	Site.menu = new FloatingMenu($('div.menu'), $('section.about'));
 
 	//Scroll Function
 	$('a[href*=#]').bind('click', function(e) {
